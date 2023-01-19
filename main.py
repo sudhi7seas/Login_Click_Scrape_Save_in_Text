d7f9252b31d36098fb81d745b618bd308bc8dbc1
sudhi777 <87425107+sudhi7seas@users.noreply.github.com>
2023-01-19 08:44:23 +0000

    This script performs the login to the 'url' and then clicks on 'home' button. Then scrapes the dynamic value that changes every 2 seconds then saves the value in a text file. The saved text files will have the current date and time.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 
-#import time module as whenever the browser is loaded it wont load immediately the dynamic variables
+#import time module because whenever the browser is loaded it wont load immediately the dynamic variables
 import time
+from datetime import datetime as dt
 
+#step 1:
 def get_driver():
 
   #set options to make the browsing easier
@@ -29,26 +31,46 @@ def get_driver():
   driver.get("http://automated.pythonanywhere.com")
   return driver
 
-# to cleant the texgt and get the value of only the nu,ber in output
+#step 3:
+# to cleant the text and get the value of only the number in output
 def clean_text(text):
   """Extract only yhe temperature value from text"""
   output = float(text.split(": ")[1])
   return output
-  
 
+#step 4:
+#to write input text into a text file
+def write_file(text):
+  """Write Input text into a text file"""
+  #Define file name such that it gives the current date time and hence here 
+  #'dt.now().strftime('Y-%m-%d.%H-%M-%S')' has been used
+  filename = f"{dt.now().strftime('%Y-%m-%d.%H-%M-%S')}.txt"
+
+  with open (filename, 'w') as file:
+    file.write(text)
+    
+  
+#step 2:
 def main():
   driver = get_driver()
-  #whenever the browser is loaded it wont load immediately the dynamic variables, hence use time.sleep(2)
-  time.sleep(2)
+
+  #As we need to get the temperature value multiple times we need to put it 
+  #inside  a while loop
+  while True:
+    
+    #whenever the browser is loaded it wont load immediately the dynamic 
+    #variables, hence use time.sleep(2)
+    time.sleep(2)
   
-  #find element by xpath is not supported and hence here find element has been 
-  #used with "by" and "value" arguments. In Google chrome, then select the particular text 
-  #then right click and give inspect. Then select the highlighted code in the developer 
-  #window again
-  # right click then select 'copy full xpath', then paste the copied value in "value"
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
-  #to get the actual text in the console, "element.text" is required
-  return clean_text(element.text)
-  
+    #find element by xpath is not supported and hence here find element has been 
+    #used with "by" and "value" arguments. In Google chrome, then select the 
+    #particular text then right click and give inspect. Then select the 
+    #highlighted code in the developer window again right click then select 'copy 
+    #full xpath', then paste the copied value in "value"
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    #to get the actual text in the console, "element.text" is required
+    text = str(clean_text(element.text))
+    write_file(text)
+    
 print(main())
 
